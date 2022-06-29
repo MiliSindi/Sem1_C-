c9566a5db93a6d9a3abc1635a3b993915dabab4d
Liudmila <sindeeva290998@gmail.com>
2022-06-29 22:40:01 +0300

    Sem1_Ex8

---
+++ Sem1_Ex8/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N/
+int numberN = 10;
+int numberB = 0;
+while (numberB<(numberN-1))
+{
+    numberB+=2;
+Console.WriteLine(numberB);
+}
+
