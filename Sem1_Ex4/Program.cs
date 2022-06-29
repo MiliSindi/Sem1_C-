907ae7fdd80c7c021d6a6fac7698d31d4d3f3118
Liudmila <sindeeva290998@gmail.com>
2022-06-29 14:41:15 +0300

    Sem1_Ex4

---
+++ Sem1_Ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает 3 числа, а на выход выдает большее из них.
+int numberA = 2;
+int numberB = 5;
+int numberC = 7;
+int numberMax = 0;
+
+if (numberA > numberMax)
+{
+numberMax = numberA;
+}
+else if (numberB > numberMax)
+{
+numberMax = numberB;
+}
+else if (numberC > numberMax)
+{
+numberMax = numberC;
+}
+else
+{
+  Console.WriteLine(numberMax);  
+}
