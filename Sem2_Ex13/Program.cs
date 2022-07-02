c2714ce0548b73ee09068b61d9a048c2d922f925
Liudmila <sindeeva290998@gmail.com>
2022-07-03 00:31:52 +0300

    Sem2_Ex13

---
+++ Sem2_Ex13/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая на выходе выдат треью цифру числа.
+System.Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 100 );
+{
+    Console.WriteLine (number % 1000 /100);
+}
