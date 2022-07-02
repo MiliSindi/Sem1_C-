da4746f212247783443dc58a8f9f6f3dfdc6860a
Liudmila <sindeeva290998@gmail.com>
2022-07-03 00:12:18 +0300

    добавили Sem2_Ex10

---
+++ Sem2_Ex10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает трехзначное значение, а на выходе выдает вторую цифру этого числа
+System.Console.WriteLine ("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000);
+{
+    Console.WriteLine (number % 100 /10);
+}
