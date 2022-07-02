54c3c1497acd05b79e2bffda81ad96737b9b4fc8
Liudmila <sindeeva290998@gmail.com>
2022-07-03 00:45:29 +0300

    Sem2_Ex_15

---
+++ Sem2_Ex_15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу,которая принимает на ввод цифру, обозначающую день ндли и определяет, является ли этот день выходным
+System.Console.WriteLine("Введите цифру от 1 до 7, соответсвующую дню недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (  number > 0 && number < 6 );
+{
+    Console.WriteLine ("будний");
+}
+if (number > 5 && number < 8)
+{
+    Console.WriteLine ("выходной");
+}
