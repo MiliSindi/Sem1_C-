03699236f1cb24b76b3d42e848dfe76390fff908
Liudmila <sindeeva290998@gmail.com>
2022-07-05 15:17:52 +0300

    Sem3_Ex21

---
+++ Sem3_Ex21/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и 
+// находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+void printGipo (double ax, double ay,double az,double bx, double by,double bz )
+{
+    double deltax = ax - bx;
+    double deltay = ay - by;
+    double deltaz = ax - bz;
+    double c = Math.Sqrt(deltax*deltax+deltay*deltay+deltaz*deltaz);
+    System.Console.WriteLine(c);
+}
+printGipo (2,8,7,-8,-3,0);
