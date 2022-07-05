f83dc8dd3fd0b331f7a57282a05e76364d1e5d34
Liudmila <sindeeva290998@gmail.com>
2022-07-05 17:17:58 +0300

    Sem3_Ex19

---
+++ Sem3_Ex19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход пятизначное 
+//число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+
+ void Palindrom (double a, double j, double x, double y, double z)
+        {
+            if (j == y && a == z) 
+            {
+                Console.WriteLine("да");
+            }
+            else
+            {
+                Console.WriteLine("нет");
+            }
+        }
+        Palindrom(2, 2, 3, 2, 1);
+    
